049585399306fa3fa8777735c5e3f8115d939994
samedan <same.dan@gmail.com>
2020-01-19 12:55:08 +0100

    working on chekout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.components';
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart_item/cart-item.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropdown = ({ cartItems, history, dispatch }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   // memoization
   cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './checkout-item.styles.scss';
+
+const CheckoutItem = () => (
+  <div className="checkout-item">
+    <div className="image-container">
+      <img src="" alt="Item" />
+    </div>
+    <span className="name"></span>
+    <span className="quantity"></span>
+    <span className="price"></span>
+    <div className="remove-button">&#10005;</div>
+  </div>
+);
+
+export default CheckoutItem;
