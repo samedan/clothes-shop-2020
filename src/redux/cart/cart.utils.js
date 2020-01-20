349334935937fcc2ecb1675c788d05ea940eb8c7
samedan <same.dan@gmail.com>
2020-01-20 10:30:30 +0100

    addItem RemoveItem to checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // first time an item is added to cart the 'quantity' is added
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const rmoveItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  // quantity is 1 by default
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
