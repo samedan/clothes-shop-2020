b2fbde2514d66e3049648b0dc6daafe5dfd3a042
samedan <same.dan@gmail.com>
2020-01-20 16:30:03 +0100

    directory & shop data into own reducers

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  snickers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,8 +8,16 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = collectionUrlParam =>
-  createSelector([selectCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+    // collections.find(
+    //   collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    // )
   );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  // ["hats", "jackets"]
+  collections => Object.keys(collections).map(key => collections[key])
+);
